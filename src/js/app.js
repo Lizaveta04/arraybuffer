235061fe62ab369e9e792e1055986681b4c78f14
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2022-02-07 15:51:38 +0300

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,9 @@ import getBuffer from './basic';
 import ArrayBufferConverter from './arraybufferconverter';
 
 console.log('app worked');
+
+const newBuff = new ArrayBufferConverter();
+newBuff.load(getBuffer());
+const result = newBuff.toString();
+console.log(result);
+console.log(typeof result);
